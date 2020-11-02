1b3f3078fc2925791601267362ecbdb37da6f8ef
Yuriy Sverchkov <yuriy.sverchkov@gmail.com>
2020-11-02 11:14:13 -0600

    Version bump for new API plan

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with open("oldest") as fh:
 
 setup(
     name="generalizedtrees",
-    version="0.0.6",
+    version="1.0.0",
     author="Yuriy Sverchkov",
     author_email="yuriy.sverchkov@wisc.edu",
     description="Library for tree models: decision trees, model trees, mimic models, etc.",
