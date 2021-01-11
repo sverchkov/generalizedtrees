f69a1e0218f19f03d7ecd822c6b0e651efe06aaa
Yuriy Sverchkov <yuriy.sverchkov@gmail.com>
2021-01-11 03:53:09 +0000

    version bump

--- generalizedtrees/__init__.py
+++ generalizedtrees/__init__.py
@@ -1,6 +1,6 @@
 # Licensed under the BSD 3-Clause License
 # Copyright (c) 2020, Yuriy Sverchkov
 
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 __author__ = 'Yuriy Sverchkov'
 __license__ = 'BSD (3 clause)'
