0b4e5a9bcad585a749ceca443ac9f05cce697962
Yuriy Sverchkov <yuriy@biostat.wisc.edu>
2021-10-05 21:54:18 +0000

    clearer printout

--- generalizedtrees/vis/text.py
+++ generalizedtrees/vis/text.py
@@ -11,13 +11,15 @@ class TreePrinter:
     def show(self, tree: Tree):
 
         def show_node(node_obj):
+            lc = node_obj.local_constraint
+            lc_str = str(lc) if lc else 'Root'
             if node_obj.split is None:
                 if node_obj.model is None:
                     logger.critical('Malformed node encountered in tree printing.')
                     return "Malformed node"
                 else:
-                    return str(node_obj.model)
+                    return f'{lc_str}: {str(node_obj.model)}'
             else:
-                return str(node_obj.split)
+                return lc_str #f'{lc_str}: {str(node_obj.split)}'
 
         return tree_to_str(tree, show_node)
